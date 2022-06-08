aedd186284c773027c3c855081c234a7584dd158
Dorigon Granato <granatos@gmail.com>
2022-06-07 22:50:47 -0300

    Primeiro Commit

---
+++ FluentValidation.Entity/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentValidation.Entity
+{
+	public class Pessoa
+	{
+        [Required]
+        public string Nome { get; set; }
+    }
+}
