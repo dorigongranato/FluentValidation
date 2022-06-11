fb40b7ffe54128706d04a040d0697e732a7c1b49
DorigonGranato <granatos@gmail.com>
2022-06-11 12:20:44 -0300

    Alterando o objeto de responde quando ocorre um erro

---
+++ FluentValidation/Filter/CustomValidationAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FluentValidation.API.Filter
+{
+    public class CustomValidationAttribute : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // our code before action executes
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // our code after action executes
+        }
+    }
+}
+
